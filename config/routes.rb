44541234efe8d327a3a4b4dd8e305b646915efb7
jattoabdul <jattoade@gmail.com>
2020-04-13 20:15:29 +0100

    rewrite route to use only a single namespace entry point

--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,9 @@ Rails.application.routes.draw do
     end
 
     namespace :api, defaults: { format: :json } do
-        scope :v1 do
-          mount_devise_token_auth_for 'User', at: 'auth'
-        end
-    end
-
-    namespace :api, defaults: { format: :json } do
         namespace :v1 do
           # API v1 routes go here
+          mount_devise_token_auth_for 'User', at: 'auth'
         end
     end
 
