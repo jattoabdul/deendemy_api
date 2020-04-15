646c818fdf5b29c9cd0828f76c5efdf0e8e2edc9
jattoabdul <jattoade@gmail.com>
2020-04-15 10:36:39 +0100

    remove comment about ruby guide

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # Sidekiq GUI for job monitoring
     if Rails.env.development?
       require 'sidekiq/web'
