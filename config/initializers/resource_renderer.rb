567ec5128c2ba8c5504ab2d7863183d561f9c4b8
jattoabdul <jattoade@gmail.com>
2020-04-16 00:50:09 +0100

    remove resource renderer

--- config/initializers/resource_renderer.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Renderers.add :resource do |resource, options|
-  serialize_options = Hash(options).slice(:include, :fields, :class_name)
-  self.content_type ||= Mime[:json]
-  SerializableResource.serialize(resource, serialize_options).to_json
-end
