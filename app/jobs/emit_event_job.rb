8515afb1390f59f5ae4c8e9aae4095e03e8e47d0
jattoabdul <jattoade@gmail.com>
2020-04-16 16:15:12 +0100

    add emit to action cable job and create event job

---
+++ app/jobs/emit_event_job.rb
@@ -0,0 +1,13 @@
+class EmitEventJob
+  include Sidekiq::Worker
+  sidekiq_options queue: :events, retry: 3
+
+  # @param id [String] Event ID
+  # @param dispatcher [String]
+  def perform(id, dispatcher)
+    # Ensure uncached query of event
+    event = Event.find(id)
+    dispatcher = dispatcher.constantize.new
+    dispatcher.emit(event)
+  end
+end
