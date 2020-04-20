01599d59002f546e1290e2f5fc1c7fdd40976dc1
jattoabdul <jattoade@gmail.com>
2020-04-20 11:55:41 +0100

    add actioncable events dispatcher and jobs for messages created

---
+++ app/lib/deendemy/message_action_cable_dispatcher.rb
@@ -0,0 +1,21 @@
+module Deendemy
+  class MessageActionCableDispatcher
+    # @param event [Event]
+    # @return [True]
+    def emit(message)
+      # Emit new message action cable event for message.receiver for this message.conversation.id
+      payload = {
+        conversation_id: message.conversation.id,
+        body: message.body,
+        sender: message.sender,
+        receiver: message.receiver
+      }
+      ActionCable.server.broadcast(build_conversation_id(message.conversation.id), payload.as_json)
+      true
+    end
+
+    def build_conversation_id(id)
+      "Conversation-#{id}"
+    end
+  end
+end
