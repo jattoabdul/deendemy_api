4d8e73516e214b339eb84913f18440cc37daea6d
jattoabdul <jattoade@gmail.com>
2020-04-21 16:24:03 +0100

    set default from and reply to for action mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,12 @@
 class ApplicationMailer < ActionMailer::Base
-    include ActiveSupport::NumberHelper
+  include ActiveSupport::NumberHelper
 
+  app_name_with_email = %("#{Deendemy::Settings.name}" <#{Deendemy::Settings.support_email}>)
+    
   default(
-    from: Deendemy::Settings.email,
-    reply_to: DeenDemy::Settings.email
+    from: app_name_with_email,
+    reply_to: app_name_with_email
   )
+
   layout 'mailer'
 end
